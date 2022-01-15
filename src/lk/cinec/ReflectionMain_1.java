c09b574eefaaaf3c3ace07dfbbcf816faf2582ce
Nadeesh <nadeeshwijesingha@gmail.com>
2022-01-16 00:48:19 +0530

    simple output

---
+++ src/lk/cinec/ReflectionMain_1.java
@@ -0,0 +1,8 @@
+package lk.cinec;
+
+public class ReflectionMain_1 {
+    public static void main(String[] args) {
+        Primary primary = new Primary();
+        System.out.println(primary);
+    }
+}
