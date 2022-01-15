154d5e8f672ccb4ceb86bda6a7f1eac33dde39e4
Nadeesh <nadeeshwijesingha@gmail.com>
2022-01-16 01:27:10 +0530

    Find object class

---
+++ src/lk/cinec/ReflectionMain_3.java
@@ -0,0 +1,12 @@
+package lk.cinec;
+
+public class ReflectionMain_3 {
+    public static void main(String[] args) {
+
+        Primary primary = new Primary();
+
+        System.out.println(primary.getClass());
+        System.out.println(primary.getClass().getName());
+
+    }
+}
